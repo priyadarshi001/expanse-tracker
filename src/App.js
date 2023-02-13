d67d2da6042359246a52f4de6fd0f6add98b1834
Rahul Priyadarshi <rahulpriyadarshi001@gmail.com>
2023-02-13 11:48:18 +0530

    Repeated styling - changed in next commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 100,
+      date: new Date(2022, 12, 25),
+    },
+    { id: "e2", title: "New TV", amount: 5600, date: new Date(2022, 11, 6) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 6000,
+      date: new Date(2023, 2, 3),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 3500,
+      date: new Date(2023, 1, 12),
+    },
+  ];
+
   return (
     <div className="App">
-      
+      <h2>Let's get started!</h2>
+      <Expenses items={expenses} />
     </div>
   );
 }
---
+++ src/components/ExpenseDate.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/ExpenseDate.css"
+
+export default function ExpenseDate(props) {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-day">{year}</div>
+      <div className="expense-year">{day}</div>
+    </div>
+  );
+}
